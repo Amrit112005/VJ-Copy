54fb2f374d6ced43247c29384d786e08a4854090
Amrit112005 <amrit2005pay@gmail.com>
2024-08-13 15:49:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "joyous-squid-botrout-4f14f1f7.koyeb.app/")
+URL = environ.get("URL", "joyous-squid-botrout-4f14f1f7.koyeb.app")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
